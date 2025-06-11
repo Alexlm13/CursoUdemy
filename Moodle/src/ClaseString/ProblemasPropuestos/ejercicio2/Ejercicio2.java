df4a9c27690160c3dfcf4ce85c8a4ed93561fc0a
Alexlm13 <alopmar131@g.educaand.es>
2025-06-11 10:19:55 +0200

    Realizacion ejercicio 2 ClaseString

--- Moodle/src/ClaseString/ProblemasPropuestos/ejercicio2/Ejercicio2.java
+++ Moodle/src/ClaseString/ProblemasPropuestos/ejercicio2/Ejercicio2.java
@@ -22,9 +22,54 @@ public class Ejercicio2 {
         }
         System.out.println();
 
-
         //Imprimir el último carácter de la cadena.
         char ultimo=cadena.charAt(cadena.length()-1);
         System.out.println("3. El último carácter de la cadena es: "+ultimo);
+
+        //Imprimir de forma inversa
+        System.out.print("4. Cadena inversa: ");
+        for (int i = cadena.length()-1 ; i>=0; i--) {
+            System.out.print(cadena.charAt(i));
+        }
+        System.out.println();
+
+        //Imprimir cada caracter del string con un guion
+        System.out.print("5. Caracteres con guión: ");
+        for (int i = 0; i < cadena.length(); i++) {
+            System.out.print(cadena.charAt(i));
+            if (i<cadena.length()-1 && cadena.charAt(i)!=' ' && cadena.charAt(i+1)!=' ') {
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+
+        //Imprimir la cantidad de vocales
+        int contador=0;
+        System.out.print("6. Imprimir vocales: ");
+        for (int i = 0; i < cadena.length(); i++) {
+            char c= Character.toLowerCase(cadena.charAt(i));
+            if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
+                contador++;
+            }
+        }
+        System.out.println("La cantidad de vocales es de "+contador);
+
+        //Verificar que una cadena se lee igual hacia delante que hacia atras
+        System.out.print("7. Cadena palindromo: "); 
+        String frase=cadena.toLowerCase();
+        frase=frase.replace(" ","");
+
+        boolean esPalindromo=true;
+        for (int i = 0, j=frase.length()-1; i < j; i++, j--) {
+            if (frase.charAt(i)!=frase.charAt(j)) {
+                esPalindromo=false;
+                break;
+            }
+        }
+        if (esPalindromo) {
+                System.out.println("Es un palíndromo.");
+            } else {
+                System.out.println("No es un palíndromo.");
+            }
     }
 }
